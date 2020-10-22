621aab4e44efc9b0a1546eddc71d66148f4dd9ee
Feri Fahrul <ferifahrul1@gmail.com>
2020-10-22 16:45:51 +0700

    menambahkan js registrasi

--- resources/views/backend/registrasi/create.blade.php
+++ resources/views/backend/registrasi/create.blade.php
@@ -10,45 +10,45 @@ Formulir Registrasi
     <div class="fade-in">
         <div class="row">
             <div class="col-lg-12">
-                {!! Form::open(array('route' => 'registrasi.store','method'=>'POST')) !!}
-                <div class="row">
+                <form id="form-registrasi">
+                    <div class="row">
 
-                    <div class="col-sm-4">
-                        <div data-target="#form-client" id="form-client-step" class="card bg-info card-accent-primary move">
-                            <div class="card-body text-center">
-                                <h5>1. Detail Client</h5>
+                        <div class="col-sm-4">
+                            <div data-target="#form-client" id="form-client-step" class="card bg-info card-accent-primary move">
+                                <div class="card-body text-center">
+                                    <h5>1. Detail Client</h5>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div class="col-sm-4">
-                        <div data-target="#form-pekerjaan"  id="form-pekerjaan-step" class="card-move card card-accent-primary move">
-                            <div class="card-body text-center">
-                                <h5>2. Detail Pekerjaan</h5>
+                        <div class="col-sm-4">
+                            <div data-target="#form-pekerjaan" id="form-pekerjaan-step" class="card-move card card-accent-primary move">
+                                <div class="card-body text-center">
+                                    <h5>2. Detail Pekerjaan</h5>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div class="col-sm-4">
-                        <div data-target="#form-tagihan" id="form-tagihan-step" class="card-move card card-accent-primary move">
-                            <div class="card-body text-center">
-                                <h5>3. Detail Tagihan</h5>
+                        <div class="col-sm-4">
+                            <div data-target="#form-tagihan" id="form-tagihan-step" class="card-move card card-accent-primary move">
+                                <div class="card-body text-center">
+                                    <h5>3. Detail Tagihan</h5>
+                                </div>
                             </div>
                         </div>
+
                     </div>
 
-                </div>
+                    {{-- form client --}}
+                    @include('backend.registrasi._form-client')
 
-                {{-- form client --}}
-                @include('backend.registrasi._form-client')
-                
-                {{-- form pekerjaan --}}
-                @include('backend.registrasi._form-pekerjaan')
+                    {{-- form pekerjaan --}}
+                    @include('backend.registrasi._form-pekerjaan')
 
-                {{-- form pembayaran --}}
-                @include('backend.registrasi._form-tagihan')
+                    {{-- form pembayaran --}}
+                    @include('backend.registrasi._form-tagihan')
 
-                {!! Form::close() !!}
+                </form>
             </div>
         </div>
     </div>
