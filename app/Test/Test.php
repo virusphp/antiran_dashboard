f0fbc4c164738b438c9a83ff3d9b11bc4bda1ab6
Feri Fahrul <ferifahrul1@gmail.com>
2020-10-24 23:43:48 +0700

    fix setter nominal uang tagihan

---
+++ app/Test/Test.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Test;
+
+// use App\Models\Registrasi;
+
+// class Test
+// {
+//     public function getStatusRegistrasi()
+//     {
+//         return Registrasi::with('details')
+//         ->whereHas('details',function ($query) {
+//             $query->where('status_proses', 0);
+//         })->get();
+//     }
+// }
